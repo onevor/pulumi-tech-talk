3a461d6ad9ca9fe13c3e4c0c9e0a99fde5ec3c73
Dennis Lien <dennis.lien.o@gmail.com>
2024-10-10 09:35:37 +0200

    feat(startuplab): Add apigateway

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,44 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as nodePath from 'path';
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+import { provider } from './utils';
+import { statupLabLambdaRole } from './iam/roles';
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const codePath = nodePath.join(__dirname, '../dist', 'functions');
+
+const statupLabLambdaName = `${pulumi.getStack()}-startup-lambda`;
+
+const statupLabLambda = new aws.lambda.Function(
+  statupLabLambdaName,
+  {
+    name: statupLabLambdaName,
+    code: new pulumi.asset.FileArchive(codePath),
+    handler: 'startuplab.handler',
+    role: statupLabLambdaRole.arn,
+    runtime: aws.lambda.Runtime.NodeJS20dX,
+    timeout: 300,
+  },
+  {
+    provider,
+  },
+);
+
+const apiName = `${pulumi.getStack()}-frode-api`;
+const api = new awsx.classic.apigateway.API(apiName, {
+  routes: [
+    {
+      path: '/ping',
+      method: 'GET',
+      eventHandler: statupLabLambda,
+    },
+    {
+      path: '/hello',
+      method: 'GET',
+      eventHandler: statupLabLambda,
+    },
+  ],
+});
+
+export const apiUrl = api.url;
